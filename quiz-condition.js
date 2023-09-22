e523a2714a73239a9554523131c0e7d4a88db785
Jobayer75 <jobayerjoyaddar@gmail.com>
2023-09-23 00:55:53 +0800

    Js

---
+++ quiz-condition.js
@@ -0,0 +1,10 @@
+var cokePrice = 17;
+var isHot = true;
+
+if (cokePrice < 15) {
+  console.log('pepsi khabo');
+} else if (isHot == true) {
+  console.log('ami hot tea chai');
+} else {
+  console.log('pani khabo');
+}
